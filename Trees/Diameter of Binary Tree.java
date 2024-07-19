d53c1ca063f85fcf1cf5c9481f10235858a87b19
AayushiHotta <147369889+AayushiHotta@users.noreply.github.com>
2024-07-19 10:51:32 +0530

    Day 4

---
+++ Trees/Diameter of Binary Tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int diameter;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter = 0;
+        height(root);
+        return diameter;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        // update the diameter if the path through the current node is longer
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+
+        // return the height of the current node
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
