db459fe4973b3a7d3bd05b24423614fe2b8d6022
AayushiHotta <147369889+AayushiHotta@users.noreply.github.com>
2024-11-16 13:55:14 +0530

    Create postorder traversal.java

---
+++ Trees/postorder traversal.java
@@ -0,0 +1,11 @@
+public class BinaryTree{
+  public static void postorder(Node root){
+    if(root == null){
+      return;
+    }
+    postorder(root.left);
+    postorder(root.right);
+    System.out.println(root.data);
+  }
+  
+}
