41ba71d0df20806b9268b786e8f172a2b2d94118
AayushiHotta <147369889+AayushiHotta@users.noreply.github.com>
2024-12-04 11:31:10 +0530

    4 Dec 2024

--- Arrays/Second Largest.java
+++ Arrays/Second Largest.java
@@ -1,13 +1,26 @@
-public static int secondLarget(int[] arr){
-  int largest = a[0];
-  int s_largest = -1;
-  for(int i = 0;i<arr.length;i++){
-    if(arr[i] > largest){
-      s_largest = largest;
-      largest = arr[i];
-    } else if(arr[i] < largest && arr[i] > s_largest){
-      s_largest = arr[i];
-    }
-  }
-  return s_largest;
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int findSecondLargest(int n, int[] arr) {
+		// Write your code here.
+		int first = Integer.MIN_VALUE;
+		int second = Integer.MIN_VALUE;
+		if(arr.length < 2){
+			return -1;
+		}
+		for(int i = 0;i<arr.length;i++){
+			if(arr[i] > first){
+				second = first;
+				first = arr[i];
+			}else if(arr[i] != first && arr[i] > second){
+				second = arr[i];
+			}
+		}
+
+		if(second != Integer.MIN_VALUE){
+			return second;
+		} else {
+			return -1;
+		}
+	}
 }
